9f81b249fcf035c6dc4a4403dd855805d8b5e970
Pawel Lisewski <dev@palekiwi.com>
2025-12-31 15:42:29 +0800

    Remove unused

--- lua/prctl/picker.lua
+++ lua/prctl/picker.lua
@@ -5,7 +5,6 @@ M.pr_picker = function(prs, opts)
 
   local pickers = require('telescope.pickers')
   local finders = require('telescope.finders')
-  local conf = require('telescope.config').values
   local actions = require('telescope.actions')
   local action_state = require('telescope.actions.state')
   local entry_display = require('telescope.pickers.entry_display')
