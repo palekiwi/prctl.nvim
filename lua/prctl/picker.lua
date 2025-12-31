56991b0f15ddab848e0f0a422a9e704d849ac2b7
Pawel Lisewski <dev@palekiwi.com>
2025-12-31 13:14:16 +0800

    Remove branch names

--- lua/prctl/picker.lua
+++ lua/prctl/picker.lua
@@ -2,25 +2,24 @@ local M = {}
 
 M.pr_picker = function(prs, opts)
   opts = opts or {}
-  
+
   local pickers = require('telescope.pickers')
   local finders = require('telescope.finders')
   local conf = require('telescope.config').values
   local actions = require('telescope.actions')
   local action_state = require('telescope.actions.state')
   local entry_display = require('telescope.pickers.entry_display')
-  
+
   -- Create entry displayer for consistent formatting
   local displayer = entry_display.create({
     separator = "\t",
     items = {
-      { width = 6 },  -- PR number
-      { width = 40 }, -- Title
-      { width = 20 }, -- Author
-      { remaining = true }, -- Branch info
+      { width = 6 },        -- PR number
+      { width = 40 },       -- Title
+      { remaining = true }, -- Author
     },
   })
-  
+
   -- Entry maker function
   local make_display = function(entry)
     local pr = entry.value
@@ -28,10 +27,9 @@ M.pr_picker = function(prs, opts)
       string.format("#%d", pr.number),
       pr.title,
       "@" .. pr.author.login,
-      string.format("%s → %s", pr.headRefName, pr.baseRefName),
     })
   end
-  
+
   -- Create picker
   pickers.new(opts, {
     prompt_title = "Pull Requests",
@@ -50,11 +48,11 @@ M.pr_picker = function(prs, opts)
       actions.select_default:replace(function()
         local selection = action_state.get_selected_entry()
         actions.close(prompt_bufnr)
-        
+
         local pr = selection.value
         local gh = require('prctl.gh')
         local utils = require('prctl.utils')
-        
+
         -- Checkout the selected PR
         gh.checkout_pr(
           pr.number,
